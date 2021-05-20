7bafafe921a6a487643faa1975d709b45430cb9a
PooiaFerdowsi <pooia.ferdowsi.is.developer@gmail.com>
2021-05-20 12:31:37 +0430

    Update bmm.c

--- bmm.c
+++ bmm.c
@@ -1,4 +1,10 @@
 // see the bmm.py file
+/* translated of bmm.py file 
+both of bmm.py and bmm.c developed by Pooia Ferdowsi 
+<pooia.ferdowsi.is.developer@gmail.com> or the 
+<pooiaferdowsiisdeveloper@gmail.com> email
+on an Ubuntu LTS ( 20.4 ) Gnu/Linux device on the 
+Gnu generel editor */
 
 #include <stdio.h>
 
@@ -15,7 +21,8 @@ scanf("%d %d" , &num1 , &num2);
 int result = 0;
 
 for ( int counter = max(num1 , num2); counter >= 1; counter--){
-	if ( ((num1 % counter) == 0 ) && ((num2 % counter) == 0 ) ){
+	if ( ((num1 % counter) == 0 ) 
+	    && ((num2 % counter) == 0 ) ){
 		if ( result == 0)
 			result = counter;
 	}
